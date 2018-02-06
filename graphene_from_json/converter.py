4725bf532ee148633b2b23fd590556eaaa035d7e
abawchen <abaw.chen@gmail.com>
2018-02-06 12:34:41 +0800

    feat: Add converter.ClassFactory and it's corresponding test case.

--- graphene_from_json/converter.py
+++ graphene_from_json/converter.py
@@ -1,7 +1,7 @@
 import re
 
 from graphene import (ID, Boolean, Dynamic, Enum, Field, Float, Int, List,
-                      NonNull, String, UUID, is_node)
+                      NonNull, ObjectType, String, UUID, is_node)
 from textblob import Word
 
 
@@ -10,8 +10,31 @@ mapper = {
     'string': String()
 }
 
+
 def convert(name):
     s = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
     word = Word(re.sub('([a-z0-9])([A-Z])', r'\1_\2', s).lower())
     return word.singularize()
+
+
+def merge_two_dicts(x, y):
+    z = x.copy()
+    z.update(y)
+    return z
+
+
+def ClassFactory(name, fields, BaseClass=ObjectType):
+    """
+    https://stackoverflow.com/a/15247892/9041712
+    """
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            if key not in argnames:
+                raise TypeError("Argument %s not valid for %s"
+                    % (key, self.__class__.__name__))
+            setattr(self, key, value)
+        BaseClass.__init__(self, name[:-len("Class")])
+    newclass = type(name, (BaseClass,), merge_two_dicts({"__init__": __init__}, fields))
+    return newclass
 
--- graphene_from_json/tests/test_converter.py
+++ graphene_from_json/tests/test_converter.py
@@ -1,4 +1,6 @@
-from ..converter import convert
+from graphene import ID, String
+
+from ..converter import ClassFactory, convert, merge_two_dicts
 
 
 def test_convert_camel_case():
@@ -11,4 +13,25 @@ def test_convert_plural():
 
 def test_convert_camel_case_plural():
     assert convert('allDogs') == 'all_dog'
+
+
+
+def test_merge_two_dicts():
+    x = {'x': 'x'}
+    y = {'y': 1}
+    expected = {
+        'x': 'x',
+        'y': 1
+    }
+    assert dict(merge_two_dicts(x, y)) == expected
+
+
+def test_class_factory_generation():
+    fields = {
+        'id': ID(),
+        'first_name': String()
+    }
+    User = ClassFactory("User", fields)
+    assert isinstance(User.id, ID)
+    assert isinstance(User.first_name, String)
 
