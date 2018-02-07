f85b00138c296ff98485de52aa0fdfaf9766e7e3
abawchen <abaw.chen@gmail.com>
2018-02-07 17:10:43 +0800

    refactor: Wrap Model.klassilize

--- graphene_from_json/builder.py
+++ graphene_from_json/builder.py
@@ -1,12 +1,8 @@
 import json
 
 from genson import SchemaBuilder
-from graphene import Field
-from graphene.utils.str_converters import to_snake_case
 
-from .converter import convert
 from .model import Model
-from .utils import ClassFactory, to_camel_case
 from .registry import get_global_registry
 
 registry = get_global_registry()
@@ -19,7 +15,6 @@ class Builder(SchemaBuilder):
                  schema_uri='DEFAULT'):
         super(Builder, self).__init__(schema_uri)
         self.root_model_name = root_model_name
-        self.registry = registry
 
     def to_graphene_klasses(self):
         json_schema = super(Builder, self).to_schema()
@@ -32,16 +27,7 @@ class Builder(SchemaBuilder):
         # TODO: Refactor
         klasses = {}
         for model in registry.models:
-            classname = to_camel_case(model.name)
-            fields = dict(
-                (to_snake_case(kv[0]), convert(kv[1], registry)) for kv in model.fields.items()
-            )
-            Class = ClassFactory(classname, fields)
-            registry.register(model, Class)
-            klasses[classname] = Class
+            model.klassilize()
+            klasses[model.klass._meta.name] = model.klass
 
         return klasses
-
-    def serialize(self):
-        pass
-
